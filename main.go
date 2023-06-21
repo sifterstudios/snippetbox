78085fb5cc773e07bda14df5bca6a258a4e5463a
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-06-21 15:58:09 +0200

    Chapter 2, Add more routes

--- main.go
+++ main.go
@@ -10,11 +10,21 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("snippet/view", snippetView)
+	mux.HandleFunc("snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass interface{}// two parameters: the TCP network address to listen on (in this case ":4000)
 	// and the servemux we just created. If http.ListenAndServe() returns an error
