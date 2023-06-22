246e09f97acc16341ff1cc292e259df3f505fbfa
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-06-22 18:46:07 +0200

    Chapter 2, add Header().Set(), to send info to user about allowable request methods

--- main.go
+++ main.go
@@ -26,10 +26,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
 	if r.Method != "POST" {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", "POST")
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
