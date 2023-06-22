39f17a874dd3d958ca603075b24d4a8c3a3a2a10
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-06-22 18:43:08 +0200

    Chapter 2, Adding GET for snippet/create

--- main.go
+++ main.go
@@ -24,6 +24,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 func main() {
@@ -31,8 +42,8 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("snippet/view", snippetView)
-	mux.HandleFunc("snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass interface{}// two parameters: the TCP network address to listen on (in this case ":4000)
 	// and the servemux we just created. If http.ListenAndServe() returns an error
