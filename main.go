ad38d8e1d0a3c955f877481e8a1c17890b596849
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2023-06-22 19:05:49 +0200

    Chapter 2, update to http.Error() to make it neater

--- main.go
+++ main.go
@@ -26,12 +26,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
 	if r.Method != "POST" {
-		// Use the Header().Set() method to add an 'Allow: POST' header to the
-		// response header map. The first parameter is the header name, and
-		// the second parameter is the header value.
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 
